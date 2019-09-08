4e81b99482396d1027023cf4807e3624ca7103ed
Sean Nemann <seannemann@Beams-MacBook-Pro.local>
2019-09-08 11:40:34 -0400

    Replaced useState with useRef for variables in useTimer hook that do not effect end state

--- components/useTimer.js
+++ components/useTimer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { SQLite } from 'expo-sqlite';
 import { AppState } from 'react-native'
 
@@ -6,40 +6,38 @@ export default function useTimer() {
 
   const [secondsOnPage, setSecondsOnPage] = useState(0)
   const db = SQLite.openDatabase('quiqly')
-  const [date, setDate] = useState()
-  const [startTime, setStartTime] = useState()
-  const [appState, setAppState] = useState(AppState.currentState)
-  const [previousTimeOnPage, setPreviousTimeOnPage] = useState(0)
+  const instanceRef = useRef({})
 
   useEffect(() => {
+    instanceRef.current.appState = AppState.currentState
     updatePreviousTimeOnPage()
   }, [])
 
   useEffect(() => {
     const id = setInterval(() => {
       const currentTime = new Date().getTime() / 1000
-      setSecondsOnPage(Math.round(previousTimeOnPage + currentTime - startTime))
-    }, 1000)
+      setSecondsOnPage(Math.round(instanceRef.current.prevTimeOnPage + currentTime - instanceRef.current.startTime))
+    }, 100)
     return(() => {clearInterval(id)})
   })
 
   const updatePreviousTimeOnPage = () => {
     if(!db) return
     const startDateTime = new Date()
-    setStartTime(startDateTime.getTime() / 1000)
-    setDate(`${(startDateTime.getMonth() + 1)}-${startDateTime.getDate()}-${startDateTime.getFullYear()}`)
+    instanceRef.current.startTime = startDateTime.getTime() / 1000
+    instanceRef.current.date = `${(startDateTime.getMonth() + 1)}-${startDateTime.getDate()}-${startDateTime.getFullYear()}`
     let timeOnPage = 0
     db.transaction((tx) => {
       tx.executeSql('CREATE TABLE IF NOT EXISTS time_open (date TEXT PRIMARY KEY, time_on_page INT);', [], (_, resultSet) => {}, (_, error) => {
         console.log(error)
       })
-      tx.executeSql('SELECT time_on_page FROM time_open WHERE date = ?', [date], (_, { rows }) => {
+      tx.executeSql('SELECT time_on_page FROM time_open WHERE date = ?', [instanceRef.current.date], (_, { rows }) => {
         let seconds = 0
         if(rows.length > 0) {
           seconds = rows._array[0].time_on_page
         }
         timeOnPage = seconds
-        setPreviousTimeOnPage(timeOnPage)
+        instanceRef.current.prevTimeOnPage = timeOnPage
       }, (tx2, error) => {
         console.log(error)
       })
@@ -49,16 +47,16 @@ export default function useTimer() {
 
   _handleAppStateChange = (nextAppState) => {
     if (
-      appState.match(/inactive|background/) &&
+      instanceRef.current.appState.match(/inactive|background/) &&
       nextAppState === 'active'
     ) {
       updatePreviousTimeOnPage()
     } else {
       db.transaction((tx) => {
-        tx.executeSql('INSERT OR REPLACE INTO time_open (date, time_on_page) values(?, ?);', [date, secondsOnPage])
+        tx.executeSql('INSERT OR REPLACE INTO time_open (date, time_on_page) values(?, ?);', [instanceRef.current.date, secondsOnPage])
       })
     }
-    setAppState(nextAppState)
+    instanceRef.current.appState = nextAppState
   };
 
   useEffect(() => {
